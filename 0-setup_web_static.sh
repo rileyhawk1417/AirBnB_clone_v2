92ed79435ee9cb4ab8b7427acd6f5f09e5ddb202
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-10-05 15:44:14 +0200

    fix: configure symlink & file dir

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,19 +8,18 @@ sudo apt-get install nginx -y;
 # Setup the directories
 mkdir -p /data/web_static/releases/test/
 mkdir /data/web_static/shared/
-mkdir /data/web_static/current/
 
 # Create symbolic link each time script is run
-ln -sf /data/web_static/current/ /data/web_static/releases/test/
+ln -sf /data/web_static/releases/test/ /data/web_static/current/ 
 
 # Insert content into files
 echo "
 <html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
+<head>
+</head>
+<body>
+Holberton School
+</body>
 </html>
 " | sudo tee /data/web_static/releases/test/index.html
 
@@ -29,23 +28,24 @@ chown -R ubuntu:ubuntu /data/
 
 echo "
 server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root    /var/www/html;
-    index   index.html index.htm;
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-    location /redirect_me {
-        return 301 www.duckduckgo.com permanent;
-    }
-    error_page 404 /404.html;
-    location /404 {
-        root /var/www/html;
-        internal;
-    }
+	listen 80 default_server;
+	listen [::]:80 default_server;
+	root    /var/www/html;
+	index   index.html index.htm;
+	add_header X-Served-By $HOSTNAME;
+	server_name seanriley.tech www.seanriley.tech;
+	rewrite ^/redirect_me https://www.duckduckgo permanent;
+	location /hbnb_static {
+		alias /data/web_static/current;
+		try_files \$uri \$uri/ /index.html;
+		index index.html;
+	}
+
+	error_page 404 /404.html;
+	location /404 {
+		root /var/www/html;
+		internal;
+	}
 }
-" | sudo tee /etc/nginx/sites-available/default;
+" | sudo tee /etc/nginx/sites-enabled/default;
 service nginx restart;
